9099f48e0df2e9c12a80f081171780d99388cc8c
Gary Holland <garyhollandxyz@gmail.com>
2025-08-14 18:41:27 +0100

    format

--- gawk_derive/src/lib.rs
+++ gawk_derive/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{DeriveInput, parse_macro_input};
 
 /// Derive macro generating an impl of the trait Event
 #[proc_macro_derive(Event)]
