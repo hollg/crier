78036c84059d53dea589feb5492081ae81fe72fc
Gary Holland <21319237+hollg@users.noreply.github.com>
2025-08-14 09:40:21 +0100

    feat: split lib into mods, add tests (#2)
    * chore: split lib into mods
    
    * test publisher
    
    * test handler
    
    * version bump

---
+++ src/event.rs
@@ -0,0 +1,20 @@
+use std::{any, panic::RefUnwindSafe};
+
+/// An object that a Publisher can send to its subscribers
+pub trait Event: Send + Sync + Clone + RefUnwindSafe + 'static {}
+
+/// Dynamically typed event. Used internally to alow Publishers to support Handlers and Events of
+/// multiple different types.
+pub trait DynEvent: Send + Sync + RefUnwindSafe + 'static {
+    fn get_data(&self) -> &dyn any::Any;
+}
+
+// Allow handlers to identify the concrete type of any Event object.
+// This is what enables the Publisher to take events of any type. It ensures that anything
+// implementing Event is safe to pass to any DynHandler, which can decide whether or not to handle
+// the event based on its concrete type.
+impl<T: Event> DynEvent for T {
+    fn get_data(&self) -> &dyn any::Any {
+        self
+    }
+}
